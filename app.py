e09db8d05bcc9026bcc56d7a277c6a14d53173f8
anuj-raj01 <anujrajput2357@gmail.com>
2025-12-28 14:59:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ import time
 from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
 
+st.image('https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUyajczcTNpM294ejB0enp2YmFoMDZmeTh3bWg4bjlocTdxN2k3eGViZyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Ee1i8QNLQO0UnlKjr7/giphy.gif')
+
