81a18735609bd5b39649978bc13eb983ee1ed43b
anuj-raj01 <anujrajput2357@gmail.com>
2025-12-28 16:22:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,11 @@ house_price = model.predict(final_value)[0]
 with st.spinner('Predicting House price'):
   time.sleep(1)
 
-msg = f'''House price is: $ {round(house_price*100000},2) '''
+msg = f'''House price is: $ {round(house_price*100000,2)} '''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Anuj Rajput**''')
+
 
 
 
