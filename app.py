31c9ccc15d899b295bdf3a26b7f002067fa95132
anuj-raj01 <anujrajput2357@gmail.com>
2025-12-28 16:20:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,15 +30,16 @@ final_value = scaler.transform([all_value])
 
 model = st.cache(RandomForestRegressor)()
 model.fit(X,y)
-house_price = model.predict(final_value)[0]
+house_price = (model.predict(final_value)[0]
 
 with st.spinner('Predicting House price'):
   time.sleep(1)
 
-msg = f'''House price is: $ {house_price*100000} '''
+msg = f'''House price is: $ {round(house_price*100000},2) '''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Anuj Rajput**''')
+
 
 
 
